6ecec92b01cdf9afcf5af0076b6f6960f0b861c2
jayjun <jayjun.ryu@kakaocorp.com>
2020-06-22 19:04:02 +0900

    책 예제 코드

--- src/test/java/com/springboottest/www/springboottest/web/HelloControllerTest.java
+++ src/test/java/com/springboottest/www/springboottest/web/HelloControllerTest.java
@@ -1,16 +1,13 @@
 package com.springboottest.www.springboottest.web;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @WebMvcTest(HelloController.class)   //웹 API 테스트시 사용된다.
 class HelloControllerTest {
@@ -26,4 +23,22 @@ class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
+
+    @Test
+    void helloDto가_리턴된다() throws Exception {
+        String name="hello";
+        int amount = 1000;
+
+        mockMvc.perform(
+                get("/hello/dto")
+                    .param("name",name)
+                    .param("amount",String.valueOf(amount))
+        ).andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("hello"))
+                .andExpect(jsonPath("amount").value(amount));
+    }
+
+
+
+
 }
